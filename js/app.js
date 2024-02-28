e6c2b801b2c857769804774cc19346720751c0f6
BelabbasMohammadQassem <mohammadqassem.93390.b@gmail.com>
2024-02-28 18:46:51 +0100

    challenge

--- js/app.js
+++ js/app.js
@@ -6,12 +6,13 @@ const app = {
      * Initialisation de l'application
      */
     init: function() {
-        console.log("app : init");
+        console.log("theme : init");
 
         // ici, on initialise tous les autres modules (qui nécessitent de l'être) de notre application !
         destinations.init();
         newsletter.init();
         theme.init();
+        slider.init();
             // TODO : initialiser les autres modules ! (par exemple, le theme !)
     }
 }
--- js/theme.js
+++ js/theme.js
@@ -1,43 +1,59 @@
-/**
- * La fonction changeTheme() permet de passer en thème sombre si on est en thème clair, et inversement
- */
-
-let theme = {
+// un objet theme
+const theme = {
+    
     init: function() {
+        // pour debug
         console.log("theme : init");
 
-        const button_lune = document.querySelector(".icon-moon");
-        button_lune.addEventListener("click");
-
+        // on sélectionne le bouton "lune" et on ajoute un écouteur d'événement dessus
+        const button = document.getElementById("theme-switch"); // ou document.querySelector("#theme-switch")
+        button.addEventListener("click", theme.changeTheme);
+        button.addEventListener("click", theme.changeTheme);
+        const themeColor = document.querySelectorAll(".theme-button");
+        themeColor.element.addEventListener("click", handleThemeColorClick);
         
-  }
-  const lune: function(changeTheme){
+        
+        for(themeColorButton of themeColor ){
+        themeColorButton.element.addEventListener("click", theme.handleThemeColorClick);
+        console.log(themeColorButton)
+        }
+    },
 
-  }
+        handleThemeColorClick: function (event){
+        console.log(event.currentTarget.id);
+        theme.changeColorTheme();
+
+    
+    },
+    changeColorTheme: function(themeColorButton){
+        if(themeColorButton === theme-green){
+            console.log('vert')
+        }else if(themeColorButton === theme-red){
+            console.log('rouge')
+        }else if (themeColorButton === theme-blue){
+            console.log('blue')
+        };
+    },
   
-}
-function changeTheme() {
-    // En JS, quand on veut manipuler le DOM, ça se passe en 2 étapes !
-    // 1ère étape : on sélectionne / on récupère l'élément du DOM qu'on veut manipuler
-    // (pour ça, on utilise l'une des fonctions de sélection : getElementById, getElementsByClassName, querySelector, querySelectorAll, etc.)
-    // vu que le body n'a pas d'ID ou de classe, le plus simple c'est d'utiliser le sélecteur CSS `body` (avec querySelector)
-    const body = document.querySelector('body');
 
-    // on va conditionner l'ajout ou la suppression en fonction du thème actuel
-    if(body.classList.contains('theme-dark')) {
-        // la classe `theme-dark` est présente sur le body, donc on l'enlève !
-        body.classList.remove('theme-dark');
-    } else {
-        // la classe `theme-dark` n'est pas présente, donc on l'ajoute !
-        body.classList.add('theme-dark');
+    changeTheme: function() {
+        // on sélectionne le body
+        const body = document.querySelector('body');
+
+        // on va conditionner l'ajout ou la suppression en fonction du thème actuel
+        if(body.classList.contains('theme-dark')) {
+            // la classe `theme-dark` est présente sur le body, donc on l'enlève !
+            body.classList.remove('theme-dark');
+        } else {
+            // la classe `theme-dark` n'est pas présente, donc on l'ajoute !
+            body.classList.add('theme-dark');
+        }
+
+        // ou avec .toggle()
+        //body.classList.toggle('theme-dark');
+
     }
+    }
+    
+};
 
-    // // 2ème étape : on effectue la manipulation souhaitée
-    // // ici, on veut ajouter la classe `theme-dark` au body
-    // // (pour manipuler les classes d'un élément du DOM, on utilise .classList, .classList.add(), .classList.remove())
-    // body.classList.add('theme-dark');
-
-    // on pourrait se simplifier la vie en utilisant classList.toggle()
-    // toggle permet de basculer : si la classe est présente elle sera supprimée, si elle est absente elle sera ajoutée !
-    //body.classList.toggle('theme-dark');
-}
